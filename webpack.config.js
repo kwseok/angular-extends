c6476742acd8e78f3956c8e3ef0bb4f3ab123bc2
Kiwon Seok <seok.kiwon@gmail.com>
2020-04-07 10:47:49 +0900

    upgrade babel & webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,8 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /(node_modules|bower_components)/,
                 options: {
-                    presets: ['es2015'],
-                    plugins: ['transform-runtime'],
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-transform-runtime'],
                 },
             },
             {
@@ -32,8 +32,8 @@ module.exports = {
                         options: {
                             sourceMap: true,
                             transpile: {
-                                presets: ['es2015'],
-                                plugins: ['transform-runtime'],
+                                presets: ['@babel/preset-env'],
+                                plugins: ['@babel/plugin-transform-runtime'],
                             },
                         },
                     },
